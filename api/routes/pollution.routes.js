4228e2973ad9fc156f41ea09104eccbe8f566fd9
Ambre Lis <alis@cfai-formation.fr>
2025-11-04 20:24:43 +0100

    update api

--- api/routes/pollution.routes.js
+++ api/routes/pollution.routes.js
@@ -1,12 +1,8 @@
-
-
 module.exports = app => {
     const pollution = require("../controllers/pollution.controllers.js");
   
     var router = require("express").Router();
   
-
-   
     router.get("/", pollution.get);
   
     app.use('/api/pollution', router);
