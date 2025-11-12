e799c0bf011ef67ae294df2f675941343ce5a681
Ambre Lis <alis@cfai-formation.fr>
2025-11-12 09:00:38 +0100

    fix env prod

--- front/src/app/services/pollution.service.ts
+++ front/src/app/services/pollution.service.ts
@@ -21,7 +21,7 @@ export interface Pollution {
   providedIn: 'root'
 })
 export class PollutionService {
-  private apiUrl =  `${environment.apiUrl}/api/pollution`;
+  private apiUrl =  `${environment.apiUrl}/api`;
 
   private pollutionsSubject = new BehaviorSubject<Pollution[]>([]);
   pollutions$ = this.pollutionsSubject.asObservable();
--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000'
+  // apiUrl: 'http://localhost:3000'
+  apiUrl: 'https://templateweb-latest-4240.onrender.com/'
+
 };
