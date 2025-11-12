a75a2693c53551a7a98219fba84ede5955b34e10
Ambre Lis <alis@cfai-formation.fr>
2025-11-12 08:56:43 +0100

    fix env prod

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
+import { enableProdMode } from '@angular/core';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 bootstrapApplication(AppComponent, {
   providers: [provideHttpClient()]
